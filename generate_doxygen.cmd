f596a0be47662ed52383e25e0806607986ff8810
Daniel Pronych <daniel.pronych@gmail.com>
2012-10-26 16:58:46 -0600

    Removing master branch files from gh-pages branch.

--- generate_doxygen.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-REM - Calls the Doxygen documentation generation with configuration file
-if not exist docs mkdir docs
-doxygen pyromannumeral.dox
--- run_tests.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-REM - Runs the Roman Numeral Script with test data
-python src\test_roman_numeral.py
-@pause
